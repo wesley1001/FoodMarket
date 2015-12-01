f7779adfac3f7f4d35e1996b70900206267017b1
lext <937505373@qq.com>
2015-12-01 17:39:03 +0800

    Seller model

--- src/css/admin-base/theme.js
+++ src/css/admin-base/theme.js
@@ -1,7 +1,7 @@
-require("../../template_content/assets/css/style-metronic.scss");
-require("../../template_content/assets/css/style.scss");
-require("../../template_content/assets/css/style-responsive.scss");
-require("../../template_content/assets/css/plugins.scss");
-require("../../template_content/assets/css/style-responsive.scss");
-require("../../template_content/assets/css/themes/default.scss");
-require("../../template_content/assets/css/pages/login.scss");
+require("../../template_content/assets/css/style-metronic.css");
+require("../../template_content/assets/css/style.css");
+require("../../template_content/assets/css/style-responsive.css");
+require("../../template_content/assets/css/plugins.css");
+require("../../template_content/assets/css/style-responsive.css");
+require("../../template_content/assets/css/themes/default.css");
+require("../../template_content/assets/css/pages/login.css");
