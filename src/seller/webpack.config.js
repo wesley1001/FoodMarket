e22d3d2097297fad6ca6c20dac57dd0453117683
卞留念 <554565808@qq.com>
2015-12-02 22:18:16 +0800

    添加卖家登录页面

---
+++ src/seller/webpack.config.js
@@ -0,0 +1,31 @@
+var path = require('path');
+var webpack = require('webpack');
+
+module.exports = {
+    entry: {
+        'admin-login': ['./src/js/admin/login.js']
+    },
+    output: {
+        path: path.resolve(__dirname, 'public/js'),
+        filename: '[name].js',
+        publicPath: '/js/',
+        sourceMapFileName: '[file].map'
+    },
+    resolve: {
+        root: [path.join(__dirname, "/src/bower_components")]
+    },
+    module: {
+        loaders: [
+            { test: /\.js$/, loader: "babel" },
+            { test: /\.css$/, loader: "style-loader!css-loader" },
+            { test: /\.scss$/, loaders: ['style', 'css', 'sass'] },
+            { test   : /\.(ttf|eot|svg|woff(2)?)(\?[a-z=0-9\.]+)?$/, loader : 'url-loader?limit=8192'},
+            { test   : /\.(png|gif|svg|jpg)$/, loader : 'url-loader?limit=8192'}
+        ]
+    },
+    plugins: [
+        new webpack.ResolverPlugin(
+            new webpack.ResolverPlugin.DirectoryDescriptionFilePlugin("bower.json", ["main"])
+        )
+    ]
+};
