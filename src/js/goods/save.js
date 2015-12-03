44a1cf8e2e34590c9eca1be52a7d078bb72a8531
lext <937505373@qq.com>
2015-12-03 23:38:36 +0800

    提交

--- src/js/goods/save.js
+++ src/js/goods/save.js
@@ -3,23 +3,41 @@ require('../../../src/css/admin/index.js');
 
 /* js */
 require('../admin/index');
+/* webuplader */
+require('fex-webuploader/dist/webuploader.css');
 var WebUploader = require('fex-webuploader');
 
-var uploader = WebUploader.Uploader({
-    swf: '/dist/Uploader.swf',
-    accept: {
-        title: 'Images',
-        extensions: 'gif,jpg,jpeg,bmp,png',
-        mimeTypes: 'image/*'
-    },
-    pick: {
-        id: '#upload-picker',
-        multiple: true
-    },
-    fileSizeLimit: 1024 * 1024 * 3,
-    fileNumLimit: 10,
-    server: '/upload'
+require('exports?window.angular!angular');
+
+var $ = jQuery;
+$(function () {
+    var app = angular.module('app', []);
+
+    var uploader = new WebUploader.Uploader({
+        swf: '/dist/Uploader.swf',
+        accept: {
+            title: 'Images',
+            extensions: 'gif,jpg,jpeg,bmp,png',
+            mimeTypes: 'image/*'
+        },
+        pick: {
+            id: '#upload-picker',
+            multiple: true
+        },
+        fileSizeLimit: 1024 * 1024 * 3,
+        fileNumLimit: 10,
+        server: '/upload'
+    });
+
+    app.controller('UploadCtl', ['$scope', function (scope) {
+        var imgs = [];
+    }]);
+
+    angular.bootstrap(document.documentElement, ['app']);
+
 });
+
+
 
 
 
