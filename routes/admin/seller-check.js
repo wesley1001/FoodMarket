14024129fc5fbc896e6a7e4f89388dbba95d254b
appli456 <lirunzhi99@hotmail.com>
2015-12-04 15:54:35 +0800

    注册审核界面完成,因为ect网站挂了所以无法修改继承问题

--- routes/admin/seller-check.js
+++ routes/admin/seller-check.js
@@ -8,15 +8,27 @@ module.exports = (router) => {
 
     var Seller = db.models.Seller;
 
-    router.get('/adminer/seller-check-list',  function *() {
+    router.get('/adminer/checkList',  function *() {
 
         var sellers = yield Seller.findAll({
             where: {
                 status: -1
             }
         });
-        this.body = yield render('admin/goodstype/list.html', {
-            sellers
+        // console.log(sellers);
+
+        // 取出seller中的dataValues对象，显示于网页
+        var data_values = [],
+            seller;
+        for (seller in sellers) {
+            if (sellers.hasOwnProperty(seller)) {
+                data_values.push(sellers[seller].dataValues);
+            }
+        }
+
+        console.log('data_values', data_values);
+        this.body = yield render('admin/checkList.html', {
+            data_values
         });
     });
 
