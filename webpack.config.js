cc667f4ca23e9fbabb0111d5a669dce46923b257
lext <937505373@qq.com>
2015-12-05 04:27:17 +0800

    暂存

--- webpack.config.js
+++ webpack.config.js
@@ -4,7 +4,8 @@ module.exports = {
     entry: {
         'admin-index': ['./src/js/admin/index.js'],
         'goods-save': ['./src/js/goods/save.js'],
-        'goodsType': ['./src/js/goodsType/goodsType.js']
+        'goodsType': ['./src/js/goodsType/goodsType.js'],
+        'seller-order-list': ['./src/js/order/seller-order-list.js']
     },
     output: {
         path: path.resolve(__dirname, 'public/dist'),
