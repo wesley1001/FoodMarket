add7fc0b710d7e45f343bb570391652e3c10d68c
lext <937505373@qq.com>
2015-12-05 00:06:56 +0800

    goods/save

--- src/js/goods/save.js
+++ src/js/goods/save.js
@@ -15,6 +15,7 @@ require('imports?$=jquery!simple-hotkeys');
 require('imports?$=jquery!simple-uploader');
 var Simditor = require('imports?$=jquery!simditor/lib/simditor.js');
 require('exports?window.angular!angular');
+require('imports?$=jquery!jquery-validation');
 
 
 var $ = jQuery;
@@ -52,15 +53,23 @@ $(function () {
 
     app.controller('UploadCtl', ['$scope', function (scope) {
         scope.imgs = [];
-        scope.imgs.toString = function () {
-            return JSON.stringify(this);
-        };
+
+        scope.data = [];
         scope.mainImg = 0;
 
         scope.setMain = function (index) {
             scope.mainImg = index;
         };
 
+        scope.remove = function (index) {
+            scope.data.splice(index, 1);
+            scope.imgs.splice(index, 1);
+        };
+
+        scope.init = function (jobj) {
+            scope.data = JSON.parse(jobj);
+            scope.imgs = scope.data.slice();
+        };
 
         uploader.on('fileQueued', function (file) {
             uploader.makeThumb( file, function( error, ret ) {
@@ -68,11 +77,89 @@ $(function () {
                     alert('预览失败，请刷新重试');
                 } else {
                     scope.imgs.push(ret);
+                    scope.data.push(file);
                     scope.$apply();
                 }
             }, 640, 260);
         });
-        window.s = scope;
+
+        uploader.on('uploadSuccess', function (file, ret) {
+            for(var i in scope.data) {
+                var ele = scope.data[i];
+                if (ele.id && ele.id == file.id) {
+                    scope.imgs[i] = ret.file_path;
+                    break;
+                }
+            }
+        });
+
+        uploader.on('uploadFinished', function () {
+            scope.$applyAsync();
+        });
+
+
+        var $form = $('#form_sample_1');
+
+        $form.validate({
+            errorElement: 'span', //default input error message container
+            errorClass: 'help-block', // default input error message class
+            focusInvalid: false, // do not focus the last invalid input
+            ignore: "",
+            rules: {
+                title: {
+                    required: true
+                },
+                price: {
+                    required: true,
+                    number: true
+                },
+                capacity: {
+                    required: true,
+                    number: true
+                },
+                content: {
+                    required: true
+                }
+            },
+
+            messages: {
+                title: {
+                    required: '请填写标题'
+                },
+                price: {
+                    required: '请填写价格',
+                    number: '请填写数字'
+                },
+                capacity: {
+                    required: '请填写价格',
+                    number: '请填写整数'
+                }
+            },
+
+            invalidHandler: function (event, validator) {
+            },
+
+            highlight: function (element) {
+                $(element)
+                    .closest('.form-group').addClass('has-error');
+            },
+
+            unhighlight: function (element) {
+                $(element)
+                    .closest('.form-group').removeClass('has-error'); // set error class to the control group
+            },
+
+            success: function (label) {
+                label
+                    .closest('.form-group').removeClass('has-error'); // set success class to the control group
+            },
+
+            submitHandler: function (form) {
+                uploader.upload();
+                form.submit();
+            }
+        });
+
     }]);
 
 
