1c9667df0b4f99a1f058533811d001f1016b98b8
appli456 <lirunzhi99@hotmail.com>
2015-12-05 13:28:15 +0800

    换系统

--- routes/admin/seller-check.js
+++ routes/admin/seller-check.js
@@ -10,24 +10,40 @@ module.exports = (router) => {
 
     router.get('/adminer/checkList',  function *() {
 
-        var sellers = yield Seller.findAll({
+        var non_check_sellers = yield Seller.findAll({
             where: {
                 status: -1
             }
         });
-        // console.log(sellers);
+        var check_sellers = yield Seller.findAll({
+            where: {
+                status: 0
+            }
 
-        // 取出seller中的dataValues对象，显示于网页
+        });
+        // console.log(non_check_sellers);
+
+        // 取出non_check_seller中的dataValues对象，显示于网页
+        var non_data_values = [],
+            non_check_seller;
+        for (non_check_seller in non_check_sellers) {
+            if (non_check_sellers.hasOwnProperty(non_check_seller)) {
+                non_data_values.push(non_check_sellers[non_check_seller].dataValues);
+            }
+        }
+
+        // 取出check_seller中的dataValues对象，显示于网页
         var data_values = [],
-            seller;
-        for (seller in sellers) {
-            if (sellers.hasOwnProperty(seller)) {
-                data_values.push(sellers[seller].dataValues);
+            check_seller;
+        for (check_seller in check_sellers) {
+            if (check_sellers.hasOwnProperty(check_seller)) {
+                data_values.push(check_sellers[check_seller].dataValues);
             }
         }
 
         console.log('data_values', data_values);
         this.body = yield render('admin/checkList.html', {
+            non_data_values,
             data_values
         });
     });
