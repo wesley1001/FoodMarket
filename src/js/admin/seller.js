029ca0f43d043f43e08c42e90f65453f759324f8
li-rz <lirunzhi99@hotmail.com>
2015-12-06 21:42:24 +0800

    玄学问题

--- src/js/admin/seller.js
+++ src/js/admin/seller.js
@@ -4,10 +4,74 @@
 
 require('../../../src/css/admin-base/common.js');
 require('./../admin-base/core.js');
+require('../../../src/bower_components/angular/angular.min.js');
 require('../../../src/css/admin/checkList.scss');
+require('../../../src/js/angular.simple-datatables.js');
+
+(function () {
+    var check_list = angular.module('check_list', []);
+
+
+    //check_list.config([
+    //    '$routeProvider',
+    //    '$locationProvider',
+    //    function ($routeProvider, $locationProvider) {
+    //        $routeProvider.when('#check', {
+    //           controller: 'checkedCtrl'
+    //        });
+    //
+    //        $routeProvider.when('#non-check', {
+    //            controller: 'nonCheckCtrl'
+    //        })
+    //}]);
+
+
+    check_list.controller('nonCheckCtrl', function ($scope, $http) {
+        // console.log($route);
+        $http
+            .get('uncheck.json', {cache: true})
+            .success(function (data) {
+                $scope.uncheck_data = data;
+
+                //$http
+                //    .get('uncheck.json', {cache: true})
+                //    .success(function (data) {
+                //        $scope.uncheck_data = data;
+                //        console.log(data);
+                //    });
+            });
+        //console.log($route);
+
+    });
+
+
+    check_list.controller('checkedCtrl', function ($scope, $http) {
+        $http
+        .get('check.json', {cache: true})
+        .success(function (data) {
+            $scope.checked_data = data;
+        });
+    });
+
+    //check_list.run([
+    //    '$rootScope',
+    //    '$location',
+    //    function ($rootScope, $location) {
+    //        $rootScope.path = $location.path();
+    //        $rootScope.on('$routeChangeSuccess', function (newVal) {
+    //            $rootScope.path = $location.path();
+    //        });
+    //}]);
+
+    angular.bootstrap(document.documentElement, ['check_list']);
+}());
+
+
+
 //$('#seller-tab')
 //    .find('a')
 //    .click(function (event) {
 //    event.preventDefault();
 //    $(this).tab('show')
 //});
+
