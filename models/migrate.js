5b69de451d9aeedc87b17bea8a83c883b733dd0e
卞留念 <554565808@qq.com>
2015-12-07 23:12:48 +0800

    去除数据库force

--- models/migrate.js
+++ models/migrate.js
@@ -3,7 +3,7 @@ var co = require('co');
 
 
 co(function * () {
-    yield db.sync({force: true});
+    yield db.sync(/*{force: true}*/);
     console.log('finished ...');
 }).catch(function () {
     console.log(arguments);
