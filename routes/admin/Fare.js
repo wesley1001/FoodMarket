7eeaf2bdb114d07e032c5217297a036be9f8cfaf
lxc <572362732@qq.com>
2015-12-14 14:02:35 +0800

    order

--- routes/admin/Fare.js
+++ routes/admin/Fare.js
@@ -10,15 +10,28 @@ var debug = require('../../instances/debug.js');
 
 module.exports = (router) => {
 
-    var seller = db.models.Seller;
+    var Seller = db.models.Seller;
 
     router.get('/adminer/fare',  function *() {
         var id=this.query.id;
-        var seller=yield Seller.findById(1);
-
+        var seller=yield Seller.findById(id);
+        debug(seller);
         this.body = yield render('admin/Fare', {
             seller
                     });
+    });
+
+    router.post('/adminer/fare',  function *() {
+
+        var body=this.request.body;
+        var seller=yield Seller.findById(body.id);
+        debug(this.request.body);
+        seller.fare=body.fare;
+        seller.content=body.content;
+        yield seller.save();
+        this.body = yield render('admin/Fare', {
+            seller
+        });
     });
 
 
