5b78bef96530e5e88e063a2fbee4702c46f85d88
lext <937505373@qq.com>
2015-12-20 18:33:52 +0800

    wechat

--- routes/wechat.js
+++ routes/wechat.js
@@ -4,7 +4,9 @@
 var wechatClient = require('./../instances/wechat.js');
 var wechatRobot = require('wechat');
 var wechatConfig = require('./../instances/config.js').wechat;
+var log = require('./../instances/log.js');
 
+var client = new OAuth(wechatConfig.appId, wechatConfig.secret);
 module.exports = (router) => {
 
     router.get('/check', function *() {
@@ -15,8 +17,22 @@ module.exports = (router) => {
         }
     });
 
-    router.get('/wechat/auth', function *(next) {
+    router.get('wechat/redirect', function *() {
+        var url = client.getAuthorizeURL('http://139.129.18.214/wechat/auth', 'state', 'scope');
+        this.redirect(url);
+        log.info('wechat/redirect');
+    });
 
+    router.get('/wechat/auth', function *(next) {
+        var result = yield client.getAccessToken('code', function (err, result) {
+        });
+
+        log.info(result);
+        var accessToken = result.data.access_token;
+        var openid = result.data.openid;
+
+        var userInfo = yield client.getUser('openid');
+        log.info(userInfo);
     });
 
 };
