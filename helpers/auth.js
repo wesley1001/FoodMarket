7b15175f36979a82695165e2e304e9deab2ba11f
lext <937505373@qq.com>
2015-12-21 10:08:48 +0800

    wechat

--- helpers/auth.js
+++ helpers/auth.js
@@ -37,11 +37,12 @@ module.exports = {
         user = ctx.current.user;
         if (util.isNullOrUndefined(user)) {
             var token = ctx.cookies.get(cookieName);
-            console.log('cookie', token);
+
             if (util.isNullOrUndefined(token)) {
                 return null;
             }
             user = yield cache.jget(token);
+            console.log('user', user);
         }
         ctx.current.user = user;
         return user;
