01a51301f886e0a33f69788bdffe896a289d769c
lext <937505373@qq.com>
2016-01-26 22:22:42 +0800

    订单支付修改

--- src/js/phone/pay.js
+++ src/js/phone/pay.js
@@ -2,27 +2,27 @@ require('./base.js');
 
 var $ = jQuery;
 
-$(function () {
-
-    try {
-        var payInfo = JSON.parse($('#pay-info').html());
-
-        console.log(payInfo);
-
-        $('#pay').click(function () {
-            var a = WeixinJSBridge.invoke(
-                "getBrandWCPayRequest", payInfo , function(res){
-                    console.log(res);
-                    alert(JSON.stringify(arguments));
-                    if(res.err_msg == "get_brand_wcpay_request:ok" ) {
-                        console.log('paid');
-                    }
-                });
-            console.log(a);
-        });
-    } catch(ex) {
-        alert(JSON.stringify(ex));
-    }
-
-
-});
+//$(function () {
+//
+//    try {
+//        var payInfo = JSON.parse($('#pay-info').html());
+//
+//        console.log(payInfo);
+//
+//        $('#pay').click(function () {
+//            var a = WeixinJSBridge.invoke(
+//                "getBrandWCPayRequest", payInfo , function(res){
+//                    console.log(res);
+//                    alert(JSON.stringify(arguments));
+//                    if(res.err_msg == "get_brand_wcpay_request:ok" ) {
+//                        console.log('paid');
+//                    }
+//                });
+//            console.log(a);
+//        });
+//    } catch(ex) {
+//        alert(JSON.stringify(ex));
+//    }
+//
+//
+//});
