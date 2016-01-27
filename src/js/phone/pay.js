c1eb55c11ca223d9681d06fa6f88a966e8d79270
lext <937505373@qq.com>
2016-01-27 13:49:06 +0800

    订单支付修改

--- src/js/phone/pay.js
+++ src/js/phone/pay.js
@@ -1,28 +1,28 @@
 require('./base.js');
 require('../../css/phone/pay.scss');
 
-var $ = jQuery;
-
-try {
-    document.addEventListener('WeixinJSBridgeReady', function onBridgeReady() {
-
-        var payInfo = JSON.parse($('#pay-info').html());
-
-        console.log(payInfo);
-        alert('start to pay');
-
-        alert(JSON.stringify(payInfo));
-
-        //公众号支付
-        WeixinJSBridge.invoke('getBrandWCPayRequest', payInfo, function(res) {
-            console.log(res);
-            alert(JSON.stringify(res));
-            if (res.err_msg == "get_brand_wcpay_request:ok") {
-                alert('支付完成');
-            }
-        });
-    }, false);
-} catch (ex) {
-    // todo: 删除alert
-    alert(JSON.stringify(ex));
-}
+//var $ = jQuery;
+//
+//try {
+//    document.addEventListener('WeixinJSBridgeReady', function onBridgeReady() {
+//
+//        var payInfo = JSON.parse($('#pay-info').html());
+//
+//        console.log(payInfo);
+//        alert('start to pay');
+//
+//        alert(JSON.stringify(payInfo));
+//
+//        //公众号支付
+//        WeixinJSBridge.invoke('getBrandWCPayRequest', payInfo, function(res) {
+//            console.log(res);
+//            alert(JSON.stringify(res));
+//            if (res.err_msg == "get_brand_wcpay_request:ok") {
+//                alert('支付完成');
+//            }
+//        });
+//    }, false);
+//} catch (ex) {
+//    // todo: 删除alert
+//    alert(JSON.stringify(ex));
+//}
